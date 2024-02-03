e01fb54d9b487aa28f8679360f3e5858e4b026d2
Jose R. Zapata <jjrzg@hotmail.com>
2024-02-02 21:54:01 -0500

    ✏️ fix typo script

--- src/update_covid_plots/Covid19_BarChartRace_update.py
+++ src/update_covid_plots/Covid19_BarChartRace_update.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# coding: utf-8
 
 """[Actualizacion Bar Chart Race]
 
--- src/update_covid_plots/Covid19_update.py
+++ src/update_covid_plots/Covid19_update.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# coding: utf-8
 
 """[Visualizacion del Coronavirus (COVID19) Mundial]
 

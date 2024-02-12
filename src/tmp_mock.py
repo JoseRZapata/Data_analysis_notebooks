6d88e0e7ce37b383f644dbad7867301f5da1cd48
Jose R. Zapata <jjrzg@hotmail.com>
2024-02-12 08:18:01 -0500

    🗑️ clean all data to set new cruft

--- src/tmp_mock.py
+++
@@ -1,7 +0,0 @@
-def mocking_test() -> bool:
-    """
-    This function is for making pytest run.
-
-    Delete this function after first python script with code is created.
-    """
-    return True
